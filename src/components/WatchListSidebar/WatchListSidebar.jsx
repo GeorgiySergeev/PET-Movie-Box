bd7871630cda04fea29079e22f17ddc3797ddf7c
GeorgiySergeev <126503204+GeorgiySergeev@users.noreply.github.com>
2024-02-23 12:18:15 +0200

    env

--- src/components/WatchListSidebar/WatchListSidebar.jsx
+++ src/components/WatchListSidebar/WatchListSidebar.jsx
@@ -3,7 +3,7 @@ import css from './WatchListSidebar.module.css';
 const WatchListSidebar = () => {
   return (
     <div className={css.sidebar_container}>
-      <h2 className={css.title}>List</h2>
+      <h2 className={css.title}>Watchlist</h2>
     </div>
   );
 };
