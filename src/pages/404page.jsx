824d6d080cb2a8828e3ae85dbecafd8544637316
GeorgiySergeev <126503204+GeorgiySergeev@users.noreply.github.com>
2024-06-10 19:29:35 +0300

    Update 404page.jsx

--- src/pages/404page.jsx
+++ src/pages/404page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ErrorPage from '../../src/assets//default-img/404.jpg';
 
 const NotFound = () => {
-  return <img src={ErrorPage} alt="404 page" />;
+  return <img src={ErrorPage} alt="404page" />;
 };
 
 export default NotFound;
