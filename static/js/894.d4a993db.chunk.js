c6ff1608e4d7f9156dde313359d94954f57ec0d5
GeorgiySergeev <126503204+GeorgiySergeev@users.noreply.github.com>
2024-06-10 16:31:19 +0000

    Deploying to gh-pages from @ GeorgiySergeev/PET-Movie-Box@824d6d080cb2a8828e3ae85dbecafd8544637316 🚀

---
+++ static/js/894.d4a993db.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpet_project_movie_box=self.webpackChunkpet_project_movie_box||[]).push([[894],{2906:function(e,t,c){c.r(t),c.d(t,{default:function(){return p}});c(2791);var a=c.p+"static/media/404.be82f71b015fca74f1ea.jpg",n=c(184),p=function(){return(0,n.jsx)("img",{src:a,alt:"404page"})}}}]);
+//# sourceMappingURL=894.d4a993db.chunk.js.map
--- static/js/894.fadea27b.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpet_project_movie_box=self.webpackChunkpet_project_movie_box||[]).push([[894],{2906:function(e,t,c){c.r(t),c.d(t,{default:function(){return p}});c(2791);var a=c.p+"static/media/404.be82f71b015fca74f1ea.jpg",n=c(184),p=function(){return(0,n.jsx)("img",{src:a,alt:"404 page"})}}}]);
-//# sourceMappingURL=894.fadea27b.chunk.js.map
